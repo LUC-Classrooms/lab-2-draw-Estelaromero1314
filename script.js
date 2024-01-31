ef2fb300cef0deed419e151f501c8a41ea7a721b
Estelaromero1314 <157331722+Estelaromero1314@users.noreply.github.com>
2024-01-30 22:21:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ function setup(){
   createCanvas(600, 400);
 }
 
+//Head
+ellipse(300, 200, 50, 50);
+
+//Body
+ellipse(200, 200, 100, 100);
+fill(325, 222, 133);
+
+//Eyes
+ellipse(175, 120, 30, 20);
+ellipse(200, 120, 30, 20);
+
 function draw(){
   background(200);
   // add your drawing code here ...
